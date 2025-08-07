19ab676683535a57be31003549bd759e7d688cab
jenson-bookipi <jenson@bookipi.com>
2025-08-08 07:59:17 +0800

    Add useless blank functions

---
+++ src/utils/blankFunctions.js
@@ -0,0 +1,88 @@
+function      addNumbers   (    a ,   b   )     
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // variable to store first number but we already have it
+    var firstNum   =   a     ;     
+
+
+
+
+
+
+
+    // variable to store second number (totally pointless)
+    var secondNum    =    b   ;    
+
+
+
+
+
+
+
+    // sum of both numbers (wow, such complexity)
+    var sumResult     =       firstNum  +   secondNum     ;      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // finally return the sum, because why not
+    return     sumResult     ;     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
